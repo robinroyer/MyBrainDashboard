d037d6f4f73549d14b2072af1195ce6265f713ed
robinroyer <ktyroby@hotmail.fr>
2017-02-20 01:18:39 +0100

    [init] default react front

---
+++ front/src/TagSelect.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TagSelect extends Component {
+  render() {
+    return (
+      <div>
+		coucou
+      </div>
+    );
+  }
+}
+
+export default TagSelect;
---
+++ front/src/UserSelect.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserSelect extends Component {
+  render() {
+    return (
+      <div>
+		coucou
+      </div>
+    );
+  }
+}
+
+export default UserSelect;
